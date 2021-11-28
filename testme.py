9856befebd4e81ccf65c4740e1243a910e3b2d29
MLUY <40317878+MLUY@users.noreply.github.com>
2021-11-28 20:09:48 +0100

    Update testme.py

--- testme.py
+++ testme.py
@@ -1,85 +1,42 @@
-from sklearn.cluster import KMeans
-import numpy as np
-import streamlit as st
+import salabim as sim
 import matplotlib.pyplot as plt
-from matplotlib.patches import Ellipse
-import matplotlib.transforms as transforms
 
-def confidence_ellipse(x, y, ax, n_std=3.0, facecolor="none", **kwargs):
-    if x.size != y.size:
-        raise ValueError("x and y must be the same size")
-    cov = np.cov(x, y)
-    pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
-    ell_radius_x = np.sqrt(1 + pearson)
-    ell_radius_y = np.sqrt(1 - pearson)
-    ellipse = Ellipse(
-        (0, 0),
-        width=ell_radius_x * 2,
-        height=ell_radius_y * 2,
-        facecolor=facecolor,
-        **kwargs
-    )
-    scale_x = np.sqrt(cov[0, 0]) * n_std
-    mean_x = np.mean(x)
-    scale_y = np.sqrt(cov[1, 1]) * n_std
-    mean_y = np.mean(y)
-    transf = (
-        transforms.Affine2D()
-        .rotate_deg(45)
-        .scale(scale_x, scale_y)
-        .translate(mean_x, mean_y)
-    )
-    ellipse.set_transform(transf + ax.transData)
-    return ax.add_patch(ellipse)
+speed=3 #m/s
 
-@st.cache
-def data():
-    X = np.random.normal(0, 1, 1000).reshape(-1, 2)
-    return X
+class Car(sim.Component):
+    def process(self):
+        tot_dist=0
+        while True:
+            
+            start_drive=env.now()
+            # driving along the road
+            yield self.hold(sim.Normal(6,2),mode='drive')
+            end_drive=env.now()
+            dist=(end_drive-start_drive)*speed
+            
+            tot_dist=tot_dist+dist
+            holding.tally(tot_dist)
+                                                
+            # stop for a coffee
+            yield self.hold(sim.Normal(10,5),mode='break')
+            holding.tally(tot_dist)
+            
 
+env=sim.Environment(trace=True)            
+Car()
 
-X = data()
+holding=sim.Monitor('holding_time')
 
-cluster_slider = st.slider(
-    min_value=1, max_value=6, value=2, label="Number of clusters: "
-)
-kmeans = KMeans(n_clusters=cluster_slider, random_state=0).fit(X)
-labels = kmeans.labels_
+runme=st.button('click to run')
 
-selectbox = st.selectbox("Visualize confidence bounds", [False, True])
-stdbox = st.selectbox("Number of standard deviations: ", [1, 2, 3])
+if runme:
+    env.run(till=100)
 
-clrs = ["red", "seagreen", "orange", "blue", "yellow", "purple"]
+tot_dist=holding.xt()
 
-n_labels = len(set(labels))
+fig,ax=plt.subplots(figsize=(15,5),nrows=1,ncols=1)
 
-individual = st.selectbox("Individual subplots?", [False, True])
+ax.plot(tot_dist[1],tot_dist[0],label='distance driven') 
 
-if individual:
-    fig, ax = plt.subplots(ncols=n_labels)
-else:
-    fig, ax = plt.subplots()
-
-for i, yi in enumerate(set(labels)):
-    if not individual:
-        a = ax
-    else:
-        a = ax[i]
-
-    xi = X[labels == yi]
-    x_pts = xi[:, 0]
-    y_pts = xi[:, 1]
-    a.scatter(x_pts, y_pts, c=clrs[yi])
-
-    if selectbox:
-        confidence_ellipse(
-            x=x_pts,
-            y=y_pts,
-            ax=a,
-            edgecolor="black",
-            facecolor=clrs[yi],
-            alpha=0.2,
-            n_std=stdbox,
-        )
-plt.tight_layout()
 st.write(fig)
+
