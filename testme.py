cbec96c64b729a4b5df686cff0300940dac7ca51
MLUY <40317878+MLUY@users.noreply.github.com>
2021-11-28 20:50:42 +0100

    Update testme.py

--- testme.py
+++ testme.py
@@ -17,7 +17,7 @@ class Car(sim.Component):
             
             start_drive=env.now()
             # driving along the road
-            yield self.hold(sim.Normal(d1,s1),mode='drive')
+            yield self.hold(sim.Normal(self.d1,self.s1),mode='drive')
             end_drive=env.now()
             dist=(end_drive-start_drive)*speed
             
@@ -25,7 +25,7 @@ class Car(sim.Component):
             holding.tally(tot_dist)
                                                 
             # stop for a coffee
-            yield self.hold(sim.Normal(b1,s2),mode='break')
+            yield self.hold(sim.Normal(self.b1,self.s2),mode='break')
             holding.tally(tot_dist)           
 
 
@@ -40,7 +40,7 @@ standard_dev1=col2.slider('standard deviation min',5,30)
 standard_dev2=col2.slider('standard deviation min',1,10)
          
 holding=sim.Monitor('holding_time')
-Car(d1=30,s1=5,b1=5,s2=2)
+Car(d1=drive_time,s1=standard_dev1,b1=break_time,s2=standard_dev2)
 env=sim.Environment(trace=False) 
 
 if st.button('click to run'):
